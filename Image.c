334cd9ef585434274bbf17480ff471dc21b69c6f
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-06-07 23:35:57 -0300

    Read image file

--- Image.c
+++ Image.c
@@ -10,20 +10,63 @@
  *-------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Image.h"
 
 int read_pgm(Image *image, char * filePath) {
 
-    FILE *f;
+    FILE *fp;
 
-    f = fopen(filePath, "r");
-    if (!f)
+    fp = fopen(filePath, "r");
+    if (!fp)
         return 1;
-    int c;
-    while ((c = getc(f)) != EOF)
-        putchar(c);
+
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t lineSize;
+
+    //image format
+    getline(&line, &len, fp);
+    strcpy(line, image->format);
+
+    //Comment
+    getline(&line, &len, fp);
+
+    //Dimensions
+    getline(&line, &len, fp);
+    int width, height;
+    sscanf(line, "%d %d", &width, &height);
+    //    printf("Width is: %d and height is: %d\n", width, height);
+
+    image->width = width;
+    image->height = height;
+
+    //Color shades
+    getline(&line, &len, fp);
+    int colorShades;
+    sscanf(line, "%d", &colorShades);
+    //    printf("The file contains %d color shades\n", colorShades);
+
+    image->color_shades = colorShades;
+
+    image->image = malloc(sizeof (int *) * height);
+
+    for (int i = 0; i < height; i++) {
+        image->image[i] = malloc(sizeof (int) * width);
+    }
+
+    int value;
+    for (int i = 0; i < image->height; i++) {
+        for (int j = 0; j < image->width; j++) {
+            fscanf(fp, "%d", &value);
+            image->image[i][j] = value;
+            printf("%3d\n", image->image[i][j]);
+        }
+    }
     
-    fclose(f);
+    fclose(fp);
+    if (line)
+        free(line);
     return 0;
 }
 
