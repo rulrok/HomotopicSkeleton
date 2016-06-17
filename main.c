ba0bc7612b3140d0e5b8c18f05961422a93584d5
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-06-16 23:30:50 -0300

    Add method to print matrix or any of its sub-matrices

--- main.c
+++ main.c
@@ -10,10 +10,65 @@
  *-------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "Image.h"
 
 #define TRUE 1
 #define FALSE 0
+
+/*
+ * Print a matrix.
+ * This function allows to specify a sub-matrix so only that portion will be printed
+ * rows - Dimension in lines of the matrix
+ * columns - Dimension in columns of the matrix
+ * M - The bidimensional matrix
+ * num - The number of following parameters. Must be between 0 and 4 inclusive.
+ * startLine - The line which it should start printing
+ * startColumn - The columns which it should start printing
+ * endLine - The final line to be printed
+ * endColumn - The final column to be printed
+ */
+void print_matrix(size_t lines, size_t columns, int M[lines][columns], int num, ...) {
+
+    va_list list;
+
+    va_start(list, num);
+
+    int startLine = 0;
+    int startColumn = 0;
+    int endLine = lines;
+    int endColumn = columns;
+
+    switch (num) {
+        case 1:
+            startLine = va_arg(list, int);
+            break;
+        case 2:
+            startLine = va_arg(list, int);
+            startColumn = va_arg(list, int);
+            break;
+        case 3:
+            startLine = va_arg(list, int);
+            startColumn = va_arg(list, int);
+            endLine = va_arg(list, int);
+            break;
+        case 4:
+            startLine = va_arg(list, int);
+            startColumn = va_arg(list, int);
+            endLine = va_arg(list, int);
+            endColumn = va_arg(list, int);
+            break;
+    }
+
+    va_end(list);
+
+    for (int i = startLine; i < endLine; i++) {
+        for (int j = startColumn; j < endColumn; j++) {
+            printf("%d,", M[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int mask_fit(Image *Im, int mask[3][3], int centerLine, int centerColumn) {
 
@@ -80,17 +135,19 @@ int main(int argc, char** argv) {
     Image * pgm_image = malloc(sizeof (Image));
     Image * pbm_image = malloc(sizeof (Image));
 
-    read_pgm(pgm_image, "./moi.pgm");
-
-    threashold_image(pgm_image, pbm_image, pgm_image->color_shades / 3);
-
-    save_pgm(pbm_image, "./moi.pbm");
+    //    read_pgm(pgm_image, "./moi.pgm");
+    //
+    //    threashold_image(pgm_image, pbm_image, pgm_image->color_shades / 3);
+    //
+    //    save_pgm(pbm_image, "./moi.pbm");
 
     int M1[3][3] = {
         {2, 0, 0},
         {1, 1, 1},
         {2, 1, 2}
     };
+    print_matrix(3, 3, M1);
+
     int M2[3][3] = {
         2, 0, 0,
         1, 1, 1,
